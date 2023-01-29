173f4e1f5bc9b2e53e95ccd195092c484a79b086
Sajithmym <84276601+sajithmym@users.noreply.github.com>
2023-01-29 13:41:39 +0530

    Python Table

---
+++ Print Table.py
@@ -0,0 +1,21 @@
+from prettytable import *
+
+Table = PrettyTable(["Item Name","Price","Quantity","Branch"])
+
+Table.add_row(["Ice","100.00","5","Balangoda"])
+Table.add_row(["Biscuit","120.00","7","Balangoda"])
+Table.add_row(["Mango","25.00","15","Balangoda"])
+Table.add_row(["Rice","65.00","10kg","Balangoda"])
+Table.add_row(["Milk","150.00","1 L","Balangoda"])
+Table.add_row(["Ice","100.00","5","Balangoda"])
+Table.add_row(["Biscuit","120.00","7","Balangoda"])
+Table.add_row(["Mango","25.00","15","Balangoda"])
+Table.add_row(["Rice","65.00","10kg","Balangoda"])
+Table.add_row(["Milk","150.00","1 L","Balangoda"])
+Table.add_row(["Ice","100.00","5","Balangoda"])
+Table.add_row(["Biscuit","120.00","7","Balangoda"])
+Table.add_row(["Mango","25.00","15","Balangoda"])
+Table.add_row(["Rice","65.00","10kg","Balangoda"])
+Table.add_row(["Milk","150.00","1 L","Balangoda"])
+
+print("\n",Table)
